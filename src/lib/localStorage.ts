e5b3e3f5b6a184eae8aca34d404a6622c35aa026
Anish <acharyaanish920@gmail.com>
2025-01-23 16:00:36 +0545

    improve

--- src/lib/localStorage.ts
+++ src/lib/localStorage.ts
@@ -1,11 +1,12 @@
-"use client";
-
 import { columnOrder, columns, tasks } from "@/constants/constant";
 
 export const loadDataFromLocalStorage = () => {
-  const savedData = localStorage.getItem("dragDropData");
-  if (savedData) {
-    return JSON.parse(savedData);
+  if (typeof window !== "undefined") {
+    const savedData = localStorage.getItem("dragDropData");
+    if (savedData) {
+      return JSON.parse(savedData);
+    }
+  } else {
+    return { tasks, columns, columnOrder };
   }
-  return { tasks, columns, columnOrder };
 };
